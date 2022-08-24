9a626d817b9f15dbb9f8d4baecce41360dcfef6b
FcoCervantesRdz <fco.cervantesrdz@gmail.com>
2022-08-24 10:48:43 -0500

    Se crea script para descargar temperaturas

---
+++ download_temp.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import requests
+from os import remove
+
+def get_table(iso_code):
+    url = 'https://climateknowledgeportal.worldbank.org/cru/cru/timeseries/tas/annual/climatology/historical/country/'
+    page = requests.get(url+iso_code)
+    if page.status_code == 200:
+        txt = 'year' + str(page.content)[2:].replace('\\n', '\n').split('\n', 1)[1][:-1]
+        archivo = open('temporal.csv', 'w')
+        archivo.write(txt)
+        archivo.close()
+        tabla = pd.read_csv('temporal.csv', index_col=0)
+        return tabla[tabla.columns[0]].rename(iso_code)
+    else:
+        print('No se encontró', iso_code)
+
+Series = []
+codes = ['ATG', 'ARG', 'BHS', 'BRB', 'BLZ', 'BOL', 'BRA', 'CHL', 'COL', 'CRI', 'CUB', 'DMA', 'ECU', 'SLV', 'GTM', 'GUY', 'HTI', 'HND', 'JAM', 'MEX', 'NIC', 'PAN', 'PRY', 'PER', 'DOM', 'KNA', 'VCT', 'LCA', 'SUR', 'TTO', 'URY', 'VEN']
+for code in codes:
+    Series.append(get_table(code))
+remove('temporal.csv')
+print("Se ha terminado de cargar los códigos de los países")
+
+tabla = pd.concat(Series, axis = 1)
+tabla.to_csv('Temperatures.csv')
+print('Se han guardado los resultados en "Temperatures.csv"')
+input()
